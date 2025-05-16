d009fc91fd90adcde926fe32826cf3345e314f82
Chatchapon Silarak <chatchaponsil@gosoft.co.th>
2025-05-17 00:01:44 +0700

    restore previous version

--- systemappbackend/systemapp/src/main/java/com/example/systemapp/repository/ProductRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.systemapp.repository;
-
-import com.example.systemapp.entity.Product;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProductRepository extends JpaRepository<Product, String> {
-}
